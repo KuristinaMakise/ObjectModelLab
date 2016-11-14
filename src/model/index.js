c38a9af736172614e30a4d244267603cd74a6ad4
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2016-11-14 15:42:24 +0100

    final

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,134 @@
+//var SensorType = Object.freeze("TEMPERATURE":1, "HUMIDITY":2, "LIGHT":3, "SWITCH":4, "DOOR":5, "FAN_SPEED":6);
+const SensorType = {
+	TEMPERATURE: 'TEMPERATURE',
+	HUMIDITY: 'HUMIDITY',
+	LIGHT: 'LIGHT',
+	SWITCH: 'SWITCH',
+	DOOR: 'DOOR',
+	FAN_SPEED: 'FAN_SPEED',
+}
 
-/* TODO : Créer le modèle objet ici */ 
+export const decod = (fichierJSON) =>
+{
+	var fichierLongueur = fichierJSON.length;
+
+	var objets = new Array();
+
+	var Sensor = function(){
+		this.id = '';
+		this.name = '';
+		this.type = '';
+	};
+
+	var Data = function(){
+
+	};
+
+	for(let i = 0; i < fichierLongueur; i++)
+	{
+		let objetCourant = new Array();
+
+		switch(fichierJSON[i]["type"])
+		{
+			case SensorType.TEMPERATURE:
+				var Temperature = function(){
+					this.type = "";
+				}
+				Temperature.prototype = new Sensor();
+				var t = new Temperature();
+				t.id = fichierJSON[i]["id"];
+				t.name = fichierJSON[i]["name"];
+				t.type = fichierJSON[i]["type"];
+				objetCourant.push(t);
+				break;
+			case SensorType.DOOR:
+				var Door = function(){
+					this.type = "";
+				}
+				Door.prototype = new Sensor();
+				var d = new Door();
+				d.id = fichierJSON[i]["id"];
+				d.name = fichierJSON[i]["name"];
+				d.type = fichierJSON[i]["type"];
+				objetCourant.push(d);
+				break;
+			case SensorType.FAN_SPEED:
+				var Fanspeed = function(){
+					this.type = "";
+				}
+				Fanspeed.prototype = new Sensor();
+				var f = new Fanspeed();
+				f.id = fichierJSON[i]["id"];
+				f.name = fichierJSON[i]["name"];
+				f.type = fichierJSON[i]["type"];
+				objetCourant.push(f);
+				break;	
+		}
+
+		if(typeof fichierJSON[i]["data"]["values"] !== 'undefined')
+		{
+			var TimeSeries = function()
+			{
+				this.values = '';
+				this.labels = '';
+			}
+			TimeSeries.prototype = new Data();
+
+			var time = new TimeSeries();
+			time.values = fichierJSON[i]["data"]["values"];
+			time.labels = fichierJSON[i]["data"]["labels"];
+			objetCourant.push(time);
+		}
+		else if (typeof fichierJSON[i]["data"]["value"] !== 'undefined')
+		{
+			var Datum = function()
+			{
+				this.value = '';
+			}
+			fichierJSON[i]["data"]["value"];
+
+			Datum.prototype = new Data();
+
+			var datum = new Datum();
+			datum.value = fichierJSON[i]["data"]["value"];
+			objetCourant.push(datum);
+		}
+
+		objets.push(objetCourant);
+	}
+
+	return objets;
+};
+
+export const notNull = (value) =>
+{
+	if(value != '')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+};
+
+export const notNullData = (data) =>
+{
+	if(data !='')
+	{
+		if(typeof data[1].values !== 'undefined'){
+
+			if(data[1].labels != ''){
+				return true;
+			}
+			return false;
+		}
+		else
+
+			return true;
+	}
+	else
+	{
+		return false;
+	}
+};
--- test/model/index.js
+++ test/model/index.js
@@ -1,10 +1,54 @@
 import expect from 'expect';
 
-// import { ??? } from '../../src/model';
+import { decod,
+	notNull
+	 } from '../../src/model';
 
 import  { data }  from './sensors_data'
-console.log(data.length);
+/*console.log(data.length);
+console.log(data[0].id);*/
 
 describe('Sensor model tests', () => {
-  /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+    describe('notNull', () => {
+	for(let i = 0; i < decod(data).length; i++){	
+		it('Id of object '+i+' shouldn\'t be null', () => {
+			expect(notNull(decod(data)[i][0].id)).toBe(true);
+		});
+		it('Name of object '+i+' shouldn\'t be null', () => {
+			expect(notNull(decod(data)[i][0].name)).toBe(true);
+		});
+		it('Type of object '+i+' shouldn\'t be null', () => {
+			expect(notNull(decod(data)[i][0].type)).toBe(true);
+		});
+		it('Type of object '+i+' shouldn\'t be null', () => {
+			expect(notNull(decod(data)[i][1].values)).toBe(true);
+		});
+		}
+     });
+     describe('exactValue', () => {
+		for(let i = 0; i < decod(data).length; i++){
+			it('Id of object '+i+' should be equal to '+data[i].id, () => {
+				expect(decod(data)[i][0].id).toBe(data[i].id);
+			});
+			it('Name of object '+i+' should be equal to '+data[i].name, () => {
+				expect(decod(data)[i][0].name).toBe(data[i].name);
+			});
+			it('Type of object '+i+' should be equal to '+data[i].type, () => {
+				expect(decod(data)[i][0].type).toBe(data[i].type);
+			});
+			if(typeof data[i].data.value !== 'undefined'){
+				it('Type of object '+i+' should be equal to '+data[i].data.value, () => {
+					expect(decod(data)[i][1].value).toBe(data[i].data.value);
+				});
+			}
+			else if(typeof data[i].data.values !== 'undefined'){
+				it('Type of object '+i+' should be equal to '+data[i].data.values, () => {
+					expect(decod(data)[i][1].values).toBe(data[i].data.values);
+				});
+				it('Type of object '+i+' should be equal to '+data[i].data.labels, () => {
+					expect(decod(data)[i][1].labels).toBe(data[i].data.labels);
+				});
+			}
+		}
+     });
 });
